4b8f5a496db7b090683f9a1d45654f6959f89f60
Kayxue <s0990006@gmail.com>
2021-06-16 21:16:03 +0800

    2021-06-16

--- addClassroom/models.py
+++ addClassroom/models.py
@@ -9,3 +9,8 @@ class Classroom(Model):
     place = CharField("教室位置", max_length=40)
 
     description = TextField("教室描述")
+
+    logInTime = DateTimeField("新增時間", auto_now_add=True)
+
+    def __str__(self):
+        return self.name
