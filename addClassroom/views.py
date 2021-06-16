fbb90492c66ef1e7eb58e4932436f4683b3dab8b
Kayxue <s0990006@gmail.com>
2021-06-16 22:11:49 +0800

    2021-06-16

--- addClassroom/views.py
+++ addClassroom/views.py
@@ -1,7 +1,10 @@
+from urllib import request
+
 from django.shortcuts import render
 from django.views.generic import *
 from django.urls import *
 from .models import *
+from django.contrib.sites.shortcuts import get_current_site
 
 
 # Create your views here.
@@ -18,3 +21,8 @@ class AddClassroom(CreateView):
 class ClassroomList(ListView):
     model = Classroom
     template_name = "classroomList.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["domain"] = self.request.build_absolute_uri()
+        return context
